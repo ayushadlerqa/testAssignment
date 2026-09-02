2731886bcd3ce3c1789cff69bb80d63a8b46be83
ayushadlerqa <ayush@adlerqa.in>
2026-09-02 16:51:06 +0530

    Update cartPage.js

--- pageobjects/cartPage.js
+++ pageobjects/cartPage.js
@@ -6,8 +6,6 @@ class cartPage {
     get cartLink()  { return $('a.shopping_cart_link'); }
     get itemsList() { return $$('div.cart_item_label'); }
 
-    // Selector applied *relative to* a single cart item (nested lookup).
-    // Kept here with the other locators so all selectors live in one place.
     itemNameSelector = 'div.inventory_item_name';
 
     async navigateToCart() {
@@ -18,7 +16,6 @@ class cartPage {
         const items = await this.itemsList;
         const itemNames = [];
         for (const item of items) {
-            // Read only the product name, not the whole label (desc/price/Remove)
             itemNames.push(await item.$(this.itemNameSelector).getText());
         }
         return itemNames;
